2a212e7e877f0047ead4daab4ac1918255958edb
Ibrohimbek-Dev <programming012@gmail.com>
2024-02-14 06:36:27 +0900

    GoogleAuth.jsx is changed

--- src/components/Login/GoogleAuth.jsx
+++ src/components/Login/GoogleAuth.jsx
@@ -71,20 +71,22 @@ const GoogleAuth = ({ setGoogleAuth }) => {
 				autoPlay
 				muted
 				loop
-				className="w-full h-full object-contain md:object-cover bg-[#7BA05B] select-none"
+				playsInline
+				className="w-full h-full object-cover bg-[#7BA05B] select-none"
 				controls={false}
 				controlsList="nodownload nodrag noremoteplayback"
 				disablePictureInPicture
 			></video>
+
 			<div className="absolute inset-0 bg-darkOverlay flex items-center justify-center p-4">
 				<div className="w-full md:w-375 p-4 bg-lightOverlay shadow-2xl rounded-md backdrop-blur-md flex flex-col items-center justify-center">
-					<div
+					<button
 						className="flex items-center justify-center  gap-2 px-4 py-2 rounded-md bg-cardOverlay cursor-pointer hover:bg-card hover:shadow-md duration-100 ease-in-out transition-all"
 						onClick={loginWithGoogle}
 					>
 						<FcGoogle className="text-xl" />
 						<p>Google Orqalik Ro'yxatdan o'tish</p>
-					</div>
+					</button>
 				</div>
 			</div>
 		</div>
