dc86843e685c8cfa3c5f9bbb096ef08ef32d8df1
Ibrohimbek-Dev <programming012@gmail.com>
2024-02-13 10:47:21 +0900

    Popular text is changed

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -22,39 +22,39 @@ const Popular = ({ popularStories }) => {
   };
 
   return (
-    <div className="flex flex-col bg-transparent lg:flex-row w-full xsm:h-40 h-full lg:h-80">
-      <div className="lg:hidden">
-        <h1 className="text-2xl mnsm:text-xl mb-1 xsm:text-sm">
-          Eng Mashxur kitoblar
-        </h1>
-      </div>
-      <div className="hidden lg:flex transition-transform duration-500">
-        <HomeButtons
-          textName={"Eng Mashxur kitoblar"}
-          moveSliderRight={nextSlide}
-          moveSliderLeft={prevItem}
-        />
-      </div>
+		<div className="flex flex-col bg-transparent lg:flex-row w-full xsm:h-40 h-full lg:h-80">
+			<div className="lg:hidden">
+				<h1 className="text-2xl mnsm:text-xl mb-1 xsm:text-sm">
+					Eng Mashxur Xikoyalar
+				</h1>
+			</div>
+			<div className="hidden lg:flex transition-transform duration-500">
+				<HomeButtons
+					textName={"Eng Mashxur Xikoyalar"}
+					moveSliderRight={nextSlide}
+					moveSliderLeft={prevItem}
+				/>
+			</div>
 
-      <div className="flex flex-row scrollbarItemSlide p-0 sm:p-1 lg:overflow-hidden overflow-x-auto">
-        {popularStories &&
-          popularStories.map((story, index) => (
-            <div key={index} className="mx-1">
-              <CardItems
-                title={story.name}
-                imageURL={story.imageURL}
-                current={current}
-                index={index}
-                author={story.author}
-                category={story.category}
-                storyUrl={story.storyURL}
-                storyPdfUrl={story.storyPdfURL}
-              />
-            </div>
-          ))}
-      </div>
-    </div>
-  );
+			<div className="flex flex-row scrollbarItemSlide p-0 sm:p-1 lg:overflow-hidden overflow-x-auto">
+				{popularStories &&
+					popularStories.map((story, index) => (
+						<div key={index} className="mx-1">
+							<CardItems
+								title={story.name}
+								imageURL={story.imageURL}
+								current={current}
+								index={index}
+								author={story.author}
+								category={story.category}
+								storyUrl={story.storyURL}
+								storyPdfUrl={story.storyPdfURL}
+							/>
+						</div>
+					))}
+			</div>
+		</div>
+	);
 };
 
 export default Popular;
